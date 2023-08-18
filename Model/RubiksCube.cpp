63c3470499d583e82905951eef258c08c167b9ab
Satya Prakash Behera <satyaprakashbehera38@gmail.com>
2023-08-18 09:38:41 +0530

    Commit

---
+++ Model/RubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pavilion on 2023-08-18.
+//
+
+#include "RubiksCube.h"
---
+++ Model/RubiksCube.h
@@ -0,0 +1,14 @@
+//
+// Created by Pavilion on 2023-08-18.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_RUBIKSCUBE_H
+#define RUBIKS_CUBE_SOLVER_RUBIKSCUBE_H
+
+
+class RubiksCube {
+
+};
+
+
+#endif //RUBIKS_CUBE_SOLVER_RUBIKSCUBE_H
