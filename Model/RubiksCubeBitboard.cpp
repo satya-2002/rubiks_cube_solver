ac388934851fe00b62cdb4b615b7b4aec55acaea
Satya Prakash Behera <satyaprakashbehera38@gmail.com>
2023-08-26 21:43:59 +0530

    Commit Bitboard Model

--- Model/RubiksCubeBitboard.cpp
+++ Model/RubiksCubeBitboard.cpp
@@ -1,3 +1,24 @@
 //
 // Created by Pavilion on 2023-08-26.
 //
+
+#include "RubiksCube.h"
+
+class RubiksCubeBitboard: public RubiksCube {
+private:
+    uint64_t solved_side_config[6]{};
+
+    int arr[3][3] = {{0, 1, 2},
+                     {7, 8, 3},
+                     {6, 5, 4}};
+
+    uint64_t one_8 = (1 << 8) - 1, one_24 = (1 << 24) - 1;
+
+    void rotateFace(int ind) {
+        uint64_t side = bitboard[ind];
+        side = side >> (8 * 6);
+        bitboard[ind] = (bitboard[ind] << 16) | (side);
+    }
+
+
+};
